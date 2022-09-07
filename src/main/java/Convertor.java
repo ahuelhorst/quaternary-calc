d8fe13f6c762f7e38026bd0a433f8ce59afb9fb5
ahuelhorst <abbyhuelhorst@gmail.com>
2022-09-07 19:58:25 -0400

    Add base convertor methods

--- src/main/java/Convertor.java
+++ src/main/java/Convertor.java
@@ -1,3 +1,22 @@
 public class Convertor {
 
+    //convert base 4 number to base 10 number
+    public int convert(String base4) {
+        int total = 0;
+        int a = 1;
+        String[] splitArray=base4.split("");
+        for (int i=splitArray.length-1; i>=0; i--){
+            int c = Integer.parseInt(splitArray[i]);
+            int b = a * c;
+            total = total + b;
+            a = a * 4;
+        }
+        return total;
+    }
+
+    //convert base 10 number to base 4 number
+    public int revert(int base10) {
+        String s = Integer.toString(base10, 4);
+        return Integer.parseInt(s);
+    }
 }

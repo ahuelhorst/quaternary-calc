fac5a93135905712d5916abe33a8252694a04c38
ahuelhorst <abbyhuelhorst@gmail.com>
2022-09-08 20:40:18 -0400

    Add main

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    Convertor convertor = new Convertor();
+    Calculator calculator = new Calculator();
+
+    public static void calculate(){
+        String i = "123/123";
+        String[] arr = i.split("[\s+-]|[\s*]|/");
+
+
+    }
+
+    public static void main(String[] args) {
+        calculate();
+    }
+}

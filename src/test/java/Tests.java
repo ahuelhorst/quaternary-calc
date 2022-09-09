c4a5f011d36158fdadddbf338dbf55ee26522a61
Nathan Lindholm <ndlindholm@bsu.edu>
2022-09-08 20:05:14 -0400

    Test convert method

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,7 +1,15 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.IOException;
 
 public class Tests {
 
+    Convertor converter = new Convertor();
+
     @Test
-    public void test(){}
+    public void covertTest() throws IOException {
+        int decimal = converter.convert("123");
+        Assertions.assertEquals(decimal,27);
+    }
 }

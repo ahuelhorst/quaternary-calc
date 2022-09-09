2071c3bd1baafafbd9596a3d34c13030a839f669
ahuelhorst <abbyhuelhorst@gmail.com>
2022-09-09 12:17:12 -0400

    Add comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 public class Main {
-    Convertor convertor = new Convertor();
     Calculator calculator = new Calculator();
-    public int calculate(String input) {
-        String[] arr = input.split("[\s+-]|[\s*]|/");
+
+    //takes math expression(String) as param and returns answer(int)
+    public int calculateExpression(String input) {
+        String[] arr = input.split("[\s+-]|[\s*]|/"); //split input into the two numbers b/w operator
         String num1 = arr[0];
         String num2 = arr[1];
-        int answer = 0;
-        char[] chars = input.toCharArray();
+        int answer = 0; //initialize answer
+        char[] chars = input.toCharArray(); //make string input a char array to get operator
         for (char ch : chars) {
             if (ch == '+') {
                 answer = calculator.add(num1, num2);

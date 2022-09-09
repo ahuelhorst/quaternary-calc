ee16483615f99a52e61a2710895cd427331930df
lleckron <leckmanbaseball@gmail.com>
2022-09-09 14:35:19 -0400

    add actions to number buttons

--- src/main/java/CalculatorUI.java
+++ src/main/java/CalculatorUI.java
@@ -1,3 +1,5 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -9,9 +11,10 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 
 public class CalculatorUI extends javafx.application.Application {
+    TextField field = new TextField();
     @Override
     public void start(Stage primaryStage) {
-        TextField field = new TextField();
+
         field.setPrefSize(50, 70);
         BorderPane root = new BorderPane();
         TilePane tile = createTile(createAllButtons());
@@ -38,9 +41,17 @@ public class CalculatorUI extends javafx.application.Application {
         buttonArrayList.add(createButton("="));
         buttonArrayList.add(createButton("C"));
         buttonArrayList.add(createButton("0"));
+        EventHandler<ActionEvent> zero = e -> field.appendText("0");
+        buttonArrayList.get(8).setOnAction(zero);
         buttonArrayList.add(createButton("1"));
+        EventHandler<ActionEvent> one = e -> field.appendText("1");
+        buttonArrayList.get(9).setOnAction(one);
         buttonArrayList.add(createButton("2"));
+        EventHandler<ActionEvent> two = e -> field.appendText("2");
+        buttonArrayList.get(10).setOnAction(two);
         buttonArrayList.add(createButton("3"));
+        EventHandler<ActionEvent> three = e -> field.appendText("3");
+        buttonArrayList.get(11).setOnAction(three);
         return buttonArrayList;
     }
     public TilePane createTile(ArrayList<Button> buttonList){

914303da7cafd3cb3688769444d839374527d5b8
ahuelhorst <abbyhuelhorst@gmail.com>
2022-09-09 15:07:15 -0400

    Change main to take in numbers and operator as separate strings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,28 @@
+import java.lang.constant.Constable;
+
 public class Main {
     Calculator calculator = new Calculator();
 
     //takes math expression(String) as param and returns answer(int)
-    public int calculateExpression(String input) {
-        String[] arr = input.split("[-|+|*|/]"); //split input into the two numbers b/w operator
-        String num1 = arr[0];
-        String num2 = arr[1];
-        int answer = 0; //initialize answer
-        char[] chars = input.toCharArray(); //make string input a char array to get operator
-        for (char ch : chars) {
-            if (ch == '+') {
-                answer = calculator.add(num1, num2);
-            }
-            if (ch == '-') {
-                answer = calculator.subtract(num1, num2);
-            }
-            if (ch == '*') {
-                answer = calculator.multiply(num1, num2);
-            }
-            if (ch == '/') {
-                answer = calculator.divide(num1, num2);
-            }
+    public Constable calculateExpression(String num1, String operator, String num2) {
+        Constable answer = 0;
+        if (operator == "+") {
+            answer = calculator.add(num1, num2);
+        }
+        if (operator == "-") {
+            answer = calculator.subtract(num1, num2);
+        }
+        if (operator == "*") {
+            answer = calculator.multiply(num1, num2);
+        }
+        if (operator == "/") {
+            answer = calculator.divide(num1, num2);
+        }
+        if (operator == "X\u00B2") {
+            answer = calculator.square(num1);
+        }
+        if (operator == "\u221A") {
+            answer = calculator.squareRoot(num1);
         }
         return answer;
     }
